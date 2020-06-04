dc2130caae8c03ca006152c655c694d0331a5579
Robert Quintana <robertquintanatx@gmail.com>
2020-06-04 08:47:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,12 +12,12 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |key, value|
     if min_num_value == nil 
-      value = min_num_value
-      key = min_num_key
+      min_num_value = value
+      min_num_key = key
       binding.pry
     elsif value < min_num_value
-      value = min_num_value
-      key = min_num_key
+      min_num_value = value
+      min_num_key = key
     end
   end 
   
